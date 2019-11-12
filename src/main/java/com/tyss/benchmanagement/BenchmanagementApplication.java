fc54b8594229bf7ce7f1358a51077368957fee49
bharathid <dbharthi123@gmail.com>
2019-11-12 17:00:02 +0530

    https://github.com/TKDarshanTY/BenchManagement

---
+++ src/main/java/com/tyss/benchmanagement/BenchmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.tyss.benchmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyss/benchmanagement/BenchmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyss.benchmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
